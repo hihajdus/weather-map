91764e7a0983e1b6e5b6a281cdcfa403f38deb04
hihajdus <sebastian.hajdus@gmail.com>
2020-03-05 11:33:18 +0100

    routing

--- weather-map-app/src/app/app.module.ts
+++ weather-map-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    ForecastComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
